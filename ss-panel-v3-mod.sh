6a07ceac2b210c3107614453f210ff8507c132a3
神代綺凜 <jindaikirin@ykilin.moe>
2018-08-14 12:01:36 +0800

    Add requirements

--- ss-panel-v3-mod.sh
+++ ss-panel-v3-mod.sh
@@ -173,14 +173,14 @@ install_ubuntu_ssr(){
 	apt-get update -y
 	apt-get install supervisor lsof -y
 	apt-get install build-essential wget -y
-	apt-get install git -y
+	apt-get install git libssl-dev -y
 	Libtest
 	wget --no-check-certificate $libAddr
 	tar xf libsodium-1.0.13.tar.gz && cd libsodium-1.0.13
 	./configure && make -j2 && make install
 	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 	ldconfig
-	apt-get install python-pip git -y
+	apt-get install python-pip python-dev -y
 	pip install cymysql
 	cd /root
 	git clone https://github.com/YKilin/shadowsocks.git "/root/shadowsocks"
