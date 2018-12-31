e37713543b393dab7718a45f406b3b7faf2ea8f4
williamsleed <601866475@qq.com>
2018-12-31 11:17:37 +0800

    Update ss-panel-v3-mod.sh

--- ss-panel-v3-mod.sh
+++ ss-panel-v3-mod.sh
@@ -4,15 +4,16 @@
 install_ss_panel_mod_v3(){
 	yum -y remove httpd
 	yum install -y unzip zip git
+	yum update -y nss curl libcurl 
 	num=$1
 	if [ "${num}" != "1" ]; then
-		wget -c --no-check-certificate https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/lnmp1.4.zip && unzip lnmp1.4.zip && rm -rf lnmp1.4.zip && cd lnmp1.4 && chmod +x install.sh && ./install.sh lnmp
+  	  wget -c --no-check-certificate https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/lnmp1.4.zip && unzip lnmp1.4.zip && rm -rf lnmp1.4.zip && cd lnmp1.4 && chmod +x install.sh && ./install.sh lnmp
 	fi
 	cd /home/wwwroot/
 	cp -r default/phpmyadmin/ .
 	cd default
 	rm -rf index.html
-	git clone https://github.com/mmmwhy/mod.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard
+	git clone https://github.com/williamsleed/mod.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard
 	cp config/.config.php.example config/.config.php
 	chattr -i .user.ini
 	mv .user.ini public
@@ -42,6 +43,7 @@ install_ss_panel_mod_v3(){
 }
 Libtest(){
 	#自动选择下载节点
+	#原来已失效:libAddr='https://download.libsodium.org/libsodium/releases/libsodium-1.0.13.tar.gz'
 	GIT='raw.githubusercontent.com'
 	LIB='download.libsodium.org'
 	GIT_PING=`ping -c 1 -w 1 $GIT|grep time=|awk '{print $7}'|sed "s/time=//"`
@@ -52,17 +54,18 @@ Libtest(){
 	if [ "$libAddr" == "$GIT" ];then
 		libAddr='https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/libsodium-1.0.13.tar.gz'
 	else
-		libAddr='https://download.libsodium.org/libsodium/releases/libsodium-1.0.13.tar.gz'
+		libAddr='https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/libsodium-1.0.13.tar.gz'
+		
 	fi
 	rm -f ping.pl		
 }
 Get_Dist_Version()
 {
-	if [ -s /usr/bin/python3 ]; then
-		Version=`/usr/bin/python3 -c 'import platform; print(platform.linux_distribution()[1][0])'`
-	elif [ -s /usr/bin/python2 ]; then
-		Version=`/usr/bin/python2 -c 'import platform; print platform.linux_distribution()[1][0]'`
-	fi
+    if [ -s /usr/bin/python3 ]; then
+        Version=`/usr/bin/python3 -c 'import platform; print(platform.linux_distribution()[1][0])'`
+    elif [ -s /usr/bin/python2 ]; then
+        Version=`/usr/bin/python2 -c 'import platform; print platform.linux_distribution()[1][0]'`
+    fi
 }
 python_test(){
 	#测速决定使用哪个源
@@ -102,47 +105,42 @@ install_centos_ssr(){
 	fi
 	rm -rf *.rpm
 	yum -y update --exclude=kernel*	
-	yum -y install git gcc lsof lrzsz python-devel libffi-devel openssl-devel
+	yum -y install git gcc python-setuptools lsof lrzsz python-devel libffi-devel openssl-devel iptables
+	yum -y update nss curl libcurl 
 	yum -y groupinstall "Development Tools" 
 	#第一次yum安装 supervisor pip
-	if [ -z "`pip`" ]; then
-		yum -y install python-pip
-	fi
-	if [ -z "`pip list | grep setuptools`" ]; then
-		pip install setuptools -U
-	fi
-	yum -y install supervisor
+	yum -y install supervisor python-pip
 	supervisord
 	#第二次pip supervisor是否安装成功
 	if [ -z "`pip`" ]; then
-		curl -O https://bootstrap.pypa.io/get-pip.py
+    curl -O https://bootstrap.pypa.io/get-pip.py
 		python get-pip.py 
 		rm -rf *.py
 	fi
 	if [ -z "`ps aux|grep supervisord|grep python`" ]; then
-		pip install supervisor
-		supervisord
+    pip install supervisor
+    supervisord
 	fi
 	#第三次检测pip supervisor是否安装成功
 	if [ -z "`pip`" ]; then
 		if [ -z "`easy_install`"]; then
-			wget http://peak.telecommunity.com/dist/ez_setup.py
-			python ez_setup.py
+    wget http://peak.telecommunity.com/dist/ez_setup.py
+		python ez_setup.py
 		fi		
 		easy_install pip
 	fi
 	if [ -z "`ps aux|grep supervisord|grep python`" ]; then
-		easy_install supervisor
-		supervisord
+    easy_install supervisor
+    supervisord
 	fi
-	pip install pip==9.0.3
+	pip install --upgrade pip
 	Libtest
 	wget --no-check-certificate $libAddr
 	tar xf libsodium-1.0.13.tar.gz && cd libsodium-1.0.13
 	./configure && make -j2 && make install
 	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 	ldconfig
-	git clone https://github.com/Tsuk1ko/shadowsocks.git "/root/shadowsocks"
+	git clone -b manyuser https://github.com/williamsleed/shadowsocks.git "/root/shadowsocks"
 	cd /root/shadowsocks
 	chkconfig supervisord on
 	#第一次安装
@@ -169,8 +167,9 @@ install_centos_ssr(){
 		python setup.py install && cd ..
 		rm -rf python
 	fi	
-	#systemctl stop firewalld.service
-	#systemctl disable firewalld.service
+	systemctl stop firewalld.service
+	systemctl disable firewalld.service
+	cd /root/shadowsocks
 	cp apiconfig.py userapiconfig.py
 	cp config.json user-config.json
 }
@@ -178,19 +177,18 @@ install_ubuntu_ssr(){
 	apt-get update -y
 	apt-get install supervisor lsof -y
 	apt-get install build-essential wget -y
-	apt-get install git libssl-dev libffi-dev -y
+	apt-get install iptables git -y
 	Libtest
 	wget --no-check-certificate $libAddr
 	tar xf libsodium-1.0.13.tar.gz && cd libsodium-1.0.13
 	./configure && make -j2 && make install
 	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 	ldconfig
-	apt-get install python-pip python-dev -y
+	apt-get install python-pip git -y
 	pip install cymysql
 	cd /root
-	git clone https://github.com/Tsuk1ko/shadowsocks.git "/root/shadowsocks"
+	git clone -b manyuser https://github.com/williamsleed/shadowsocks.git "/root/shadowsocks"
 	cd shadowsocks
-	pip install setuptools -U
 	pip install -r requirements.txt
 	chmod +x *.sh
 	# 配置程序
@@ -200,11 +198,11 @@ install_ubuntu_ssr(){
 install_node(){
 	clear
 	echo
-	echo "#############################################################"
-	echo "# One click Install Shadowsocks-Python-Manyuser             #"
-	echo "# Github: https://github.com/mmmwhy/ss-panel-and-ss-py-mu   #"
-	echo "# Author: 91vps                                             #"
-	echo "#############################################################"
+	echo "##########################################################################"
+	echo "# One click Install Shadowsocks-Python-Manyuser                          #"
+	echo "# Github: https://github.com/williamsleed/ss-panel-and-ss-py-mu          #"
+	echo "# Author: 91vps                                                          #"
+	echo "##########################################################################"
 	echo
 	#Check Root
 	[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
@@ -224,7 +222,7 @@ install_node(){
 			release="ubuntu"
 		elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
 			release="centos"
-		fi
+	  fi
 	}
 	install_ssr_for_each(){
 		check_sys
@@ -254,45 +252,127 @@ install_node(){
 	supervisorctl shutdown
 	#某些机器没有echo_supervisord_conf 
 	wget -N -P  /etc/ --no-check-certificate  https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/supervisord.conf
-	#supervisord
+	supervisord
 	#iptables
-	#iptables -F
-	#iptables -X  
-	#iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
-	#iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
-	#iptables-save >/etc/sysconfig/iptables
-	#iptables-save >/etc/sysconfig/iptables
-	#echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
+	iptables -F
+	iptables -X  
+	iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
+	iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
+	iptables-save >/etc/sysconfig/iptables
+	iptables-save >/etc/sysconfig/iptables
+	echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
 	echo "/usr/bin/supervisord -c /etc/supervisord.conf" >> /etc/rc.local
 	chmod +x /etc/rc.d/rc.local
-	echo "#############################################################"
-	echo "# 安装完成，节点即将重启使配置生效                          #"
-	echo "# Github: https://github.com/mmmwhy/ss-panel-and-ss-py-mu   #"
-	echo "# Author: 91vps                                             #"
-	echo "#############################################################"
-	#reboot now
+	echo "###################################################################"
+	echo "# 安装完成，节点即将重启使配置生效                                #"
+	echo "# Github: https://github.com/williamsleed/ss-panel-and-ss-py-mu   #"
+	echo "# Author: 91vps                                                   #"
+	echo "###################################################################"
+	reboot now
+}
+install_node_db(){
+	clear
+	echo
+	echo "###################################################################"
+	echo "# One click Install Shadowsocks-Python-Manyuser                   #"
+	echo "# Github: https://github.com/williamsleed/ss-panel-and-ss-py-mu   #"
+	echo "# Author: 91vps                                                   #"
+	echo "###################################################################"
+	echo
+	#Check Root
+	[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+	#check OS version
+	check_sys(){
+		if [[ -f /etc/redhat-release ]]; then
+			release="centos"
+		elif cat /etc/issue | grep -q -E -i "debian"; then
+			release="debian"
+		elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+			release="ubuntu"
+		elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+			release="centos"
+		elif cat /proc/version | grep -q -E -i "debian"; then
+			release="debian"
+		elif cat /proc/version | grep -q -E -i "ubuntu"; then
+			release="ubuntu"
+		elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+			release="centos"
+	  fi
+	}
+	install_ssr_for_each(){
+		check_sys
+		if [[ ${release} = "centos" ]]; then
+			install_centos_ssr
+		else
+			install_ubuntu_ssr
+		fi
+	}
+	# 取消文件数量限制
+	sed -i '$a * hard nofile 512000\n* soft nofile 512000' /etc/security/limits.conf
+	read -p "Please input your MYSQL_HOST: " MYSQL_HOST
+	read -p "Please input your MYSQL_DB: " MYSQL_DB 
+	read -p "Please input your MYSQL_USER: " MYSQL_USER 
+	read -p "Please input your MYSQL_PASS: " MYSQL_PASS 
+	read -p "Please input your Node_ID(like:1): " UserNODE_ID
+	install_ssr_for_each
+	cd /root/shadowsocks
+	echo -e "modify Config.py...\n"
+	sed -i "s#'modwebapi'#'glzjinmod'#" /root/shadowsocks/userapiconfig.py #改成数据库对接
+	sed -i "s#'zhaoj.in'#'jd.hk'#" /root/shadowsocks/userapiconfig.py #混淆设置
+	MYSQL_HOST=${MYSQL_HOST:-"http://127.0.0.1"}
+	sed -i "s#MYSQL_HOST = '127.0.0.1'#MYSQL_HOST = '${MYSQL_HOST}'#" /root/shadowsocks/userapiconfig.py
+	MYSQL_DB=${MYSQL_DB:-"root"}
+	sed -i "s#MYSQL_DB = 'shadowsocks'#MYSQL_DB = '${MYSQL_DB}'#" /root/shadowsocks/userapiconfig.py
+	MYSQL_USER=${MYSQL_USER:-"root"}
+	sed -i "s#MYSQL_USER = 'ss'#MYSQL_USER = '${MYSQL_USER}'#" /root/shadowsocks/userapiconfig.py
+	MYSQL_PASS=${MYSQL_PASS:-"root"}
+	sed -i "s#MYSQL_PASS = 'ss'#MYSQL_PASS = '${MYSQL_PASS}'#" /root/shadowsocks/userapiconfig.py
+	UserNODE_ID=${UserNODE_ID:-"3"}
+	sed -i '2d' /root/shadowsocks/userapiconfig.py
+	sed -i "2a\NODE_ID = ${UserNODE_ID}" /root/shadowsocks/userapiconfig.py
+	# 启用supervisord
+	supervisorctl shutdown
+	#某些机器没有echo_supervisord_conf 
+	wget -N -P  /etc/ --no-check-certificate  https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/supervisord.conf
+	supervisord
+	#iptables
+	iptables -F
+	iptables -X  
+	iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
+	iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
+	iptables-save >/etc/sysconfig/iptables
+	iptables-save >/etc/sysconfig/iptables
+	echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
+	echo "/usr/bin/supervisord -c /etc/supervisord.conf" >> /etc/rc.local
+	chmod +x /etc/rc.d/rc.local
+	echo "###################################################################"
+	echo "# 安装完成，节点即将重启使配置生效                                #"
+	echo "# Github: https://github.com/williamsleed/ss-panel-and-ss-py-mu   #"
+	echo "# Author: 91vps                                                   #"
+	echo "###################################################################"
+	reboot now
 }
 install_panel_and_node(){
 	install_ss_panel_mod_v3 $1
 	# 取消文件数量限制
 	sed -i '$a * hard nofile 512000\n* soft nofile 512000' /etc/security/limits.conf
 	install_centos_ssr
-	wget -N -P  /root/shadowsocks/ --no-check-certificate  https://raw.githubusercontent.com/Tsuk1ko/ss-panel-and-ss-py-mu/master/userapiconfig.py
+	wget -N -P  /root/shadowsocks/ --no-check-certificate  https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/userapiconfig.py
 	# 启用supervisord
 	echo_supervisord_conf > /etc/supervisord.conf
-	sed -i '$a [program:ssr]\ncommand = python /root/shadowsocks/server.py\nuser = root\nautostart = true\nautorestart = true' /etc/supervisord.conf
+  sed -i '$a [program:ssr]\ncommand = python /root/shadowsocks/server.py\nuser = root\nautostart = true\nautorestart = true' /etc/supervisord.conf
 	supervisord
 	#iptables
-	#systemctl stop firewalld.service
-	#systemctl disable firewalld.service
-	#yum install iptables -y
-	#iptables -F
-	#iptables -X  
-	#iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
-	#iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
-	#iptables-save >/etc/sysconfig/iptables
-	#iptables-save >/etc/sysconfig/iptables
-	#echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
+	systemctl stop firewalld.service
+	systemctl disable firewalld.service
+	yum install iptables -y
+	iptables -F
+	iptables -X  
+	iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
+	iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
+	iptables-save >/etc/sysconfig/iptables
+	iptables-save >/etc/sysconfig/iptables
+	echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
 	echo "/usr/bin/supervisord -c /etc/supervisord.conf" >> /etc/rc.local
 	chmod +x /etc/rc.d/rc.local
 	echo "#############################################################"
@@ -305,20 +385,21 @@ install_panel_and_node(){
 	reboot now
 }
 echo
-echo "#############################################################"
-echo "# One click Install SS-panel and Shadowsocks-Py-Mu          #"
-echo "# Github: https://github.com/mmmwhy/ss-panel-and-ss-py-mu   #"
-echo "# Author: 91vps                                             #"
-echo "# Please choose the server you want                         #"
-echo "# 1  SS-V3_mod_panel and node One click Install             #"
-echo "# 2  SS-node One click Install                              #"
-echo "#############################################################"
+echo "###################################################################"
+echo "# One click Install SS-panel and Shadowsocks-Py-Mu                #"
+echo "# Github: https://github.com/williamsleed/ss-panel-and-ss-py-mu   #"
+echo "# Author: 91vps                                                   #"
+echo "# Please choose the server you want                               #"
+echo "# 1  SS-V3_mod_panel and node One click Install                   #"
+echo "# 2  SS-node modwebapi One click Install                          #"
+echo "# 3  SS-node Database  One click Install                          #"
+echo "###################################################################"
 echo
 num=$1
 if [ "${num}" == "1" ]; then
-	install_panel_and_node 1
+    install_panel_and_node 1
 else
-	stty erase '^H' && read -p " 请输入数字 [1-2]:" num
+    stty erase '^H' && read -p " 请输入数字 [1-3]:" num
 		case "$num" in
 		1)
 		install_panel_and_node
@@ -326,9 +407,11 @@ else
 		2)
 		install_node
 		;;
+		3)
+		install_node_db
+		;;
 		*)
-		echo "请输入正确数字 [1-2]"
+		echo "请输入正确数字 [1-3]"
 		;;
 	esac
 fi
-
